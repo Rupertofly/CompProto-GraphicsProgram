62141e34f941fdeeda33775c009c99477e297411
Rupertofly <rupertqmoore@me.com>
2017-04-06 21:38:43 +1000

    removed unnessecery experiments

--- pT1GP/data/myShader.glsl
+++
@@ -1,8 +0,0 @@
-uniform vec4 iResolution;
-uniform sampler2D tex;
-
-void main(){
-  vec2 uv = gl_FragCoord.xy / iResolution.xy;
-  vec4 samTex = texture2D(tex,uv);
-  gl_FragColor.xyzw = vec4(sin(uv.x*10),sin(uv.y*12),sin((uv.x*10)+1),samTex.a);
-}
